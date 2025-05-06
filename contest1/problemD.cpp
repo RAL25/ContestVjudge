56ad1fc1d73cf65ade79f775ba8f2f6538c88529
Rian Alves <rianalvesleal@gmail.com>
2025-05-06 20:04:03 -0300

    Problema D contest 1, pronto

--- contest1/problemD.cpp
+++ contest1/problemD.cpp
@@ -2,22 +2,64 @@
 #include<vector>
 #include<unordered_map>
 using namespace std;
+string padrao = "the quick brown fox jumps over the lazy dog";
+unordered_map<char, char> mapaCerto;
+
+bool podeMapear(string linha){
+    if(linha.length() != padrao.length())
+        return false;
+    unordered_map<char, char> mapaTemp;
+    unordered_map<char, char> mapaReverso;
+    for(int i=0; i<linha.length(); i++){
+        char a = linha[i];
+        char b = padrao[i];
+        if(a==' ' && b!=' ' || a!=' ' && b==' ')
+            return false;
+        if(a != ' '){
+            if(mapaTemp.count(a) && mapaTemp[a] != b)
+                return false;
+            if(mapaReverso.count(b) && mapaReverso[b] != a)
+                return false;
+            mapaTemp[a] = b;
+            mapaReverso[b] = a;
+        }
+    }
+    mapaCerto = mapaTemp;
+    return true;
+}
+
 int main(){
     int t;
     string aux;
-    string padrao = "the quick brown fox jumps over the lazy dog";
     getline(cin, aux);
     t = stoi(aux);
-    getline(cin,aux);
-    while(t--){
-        vector<string> frases;
-        bool check;
+    getline(cin, aux);
+    for(int i=0; i<t; i++){
+        vector<string> linhas;
         while(getline(cin,aux)){
             if(aux.empty()) break;
-            frases.push_back(aux);
-            
+            linhas.push_back(aux);
         }
-        if(!(t-1)) cout<<endl;
+        mapaCerto.clear();
+        bool check = false;
+        for(string linha : linhas){
+            if(podeMapear(linha)){
+                check = true;
+                break;
+            }
+        }
+        if(!check)
+            cout<<"No solution."<<endl;
+        else{
+            for(string linha : linhas){
+                for(char a : linha){
+                    if(a==' ') cout<<' ';
+                    else cout << mapaCerto[a];
+                }
+                cout<<endl;
+            }
+        }
+        if(i != t-1) cout<<endl;
     }
     return 0;
 }
